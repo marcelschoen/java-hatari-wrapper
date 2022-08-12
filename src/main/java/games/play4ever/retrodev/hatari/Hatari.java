e001e4e1db093017b9dab8d6bbee027f041c02b2
mschoen <marcelschoen@yahoo.com>
2022-08-12 22:00:42 +0200

    Added EmuTOS and support for auto-selection of EmuTOS based on JVM locale.

--- src/main/java/games/play4ever/retrodev/hatari/Hatari.java
+++ src/main/java/games/play4ever/retrodev/hatari/Hatari.java
@@ -4,6 +4,7 @@ import com.sun.jna.platform.DesktopWindow;
 import com.sun.jna.platform.WindowUtils;
 import com.sun.jna.platform.win32.*;
 import games.play4ever.retrodev.util.FileUtil;
+import games.play4ever.retrodev.util.PlatformUtil;
 import games.play4ever.retrodev.util.ZipHelper;
 
 import java.awt.*;
@@ -28,6 +29,23 @@ public class Hatari {
      * TOS versions included in this Java wrapper.
      */
     public enum TOS {
+        etos512cz,
+        etos512de,
+        etos512es,
+        etos512fi,
+        etos512fr,
+        etos512gr,
+        etos512hu,
+        etos512it,
+        etos512nl,
+        etos512no,
+        etos512pl,
+        etos512ru,
+        etos512se,
+        etos512sg,
+        etos512tr,
+        etos512uk,
+        etos512us,
         tos100,
         tos102,
         tos104,
@@ -37,6 +55,46 @@ public class Hatari {
         tos306,
         tos402,
         tos404;
+
+        /**
+         * Tries to return the EmuTOS version which corresponds to the current locale language
+         * or country code value of the running JVM.
+         *
+         * @return The EmuTOS matching the current language / country, or the US version (fallback).
+         */
+        public static TOS getEmuTOSByLocale() {
+            // 1st try by language code, e.g. "de", "it"
+            TOS value = getLocalizedEmuTOS(Locale.getDefault().getLanguage().toLowerCase());
+            if(value == null) {
+                // If that fails, try country code (e.g. "uk", "us")
+                value = getLocalizedEmuTOS(Locale.getDefault().getCountry().toLowerCase());
+            }
+            return value == null ? etos512us : value;
+        }
+
+        /**
+         * Tries to return the EmuTOS version which corresponds to the given language
+         * or country code value.
+         *
+         * @param code The 2-character country or language code
+         * @return The EmuTOS matching the current language, or the US version (fallback).
+         */
+        public static TOS getEmuTOSByCountryOrLanguage(String code) {
+            return getLocalizedEmuTOS(code);
+        }
+
+        private static TOS getLocalizedEmuTOS(String code) {
+            if(code.trim().length() != 2) {
+                throw new IllegalArgumentException("Parameter code must be a 2-character value: " +  code);
+            }
+            String tosName = "etos512" + code;
+            try {
+                return valueOf(tosName);
+            } catch(Exception e) {
+                // ignore
+            }
+            return null;
+        }
     }
 
     /**
@@ -151,7 +209,7 @@ public class Hatari {
             MODE mode = MODE.low;
             File fileToCopy = null;
 
-            prepare(new File("./hatari"), Hatari.TOS.tos206);
+            prepare(new File("./hatari"), TOS.getEmuTOSByLocale());
             startEmulator(INSTANCES.testing, machine, memory, mode, machine.hasBlitter, null, fileToCopy);
 
         } catch(Exception e) {
@@ -436,9 +494,14 @@ public class Hatari {
         File hatariBin = new File(tempDir, "hatari-win64-release.exe");
         if (!hatariBin.exists() || !hatariBin.canExecute()) {
             try {
-                System.out.println(">> Unpack emulator to: " + tempDir);
-                unpackEmulator(tempDir, "/hatari-windows.zip");
-                System.out.println(">> Emulator unpacked to: " + tempDir);
+                String emulatorArchive = PlatformUtil.getOperatingSystemType().emulatorArchive;
+                if(emulatorArchive != null) {
+                    System.out.println(">> Unpack emulator to: " + tempDir);
+                    unpackEmulator(tempDir, "/hatari-windows.zip");
+                    System.out.println(">> Emulator unpacked to: " + tempDir);
+                } else {
+                    throw new RuntimeException(">> Platform '" + PlatformUtil.getOperatingSystemType().name() + " not yet supported.");
+                }
             } catch (IOException ex) {
                 deleteDir(tempDir);
                 throw new RuntimeException("Failed to prepare the emulator: " + ex, ex);
